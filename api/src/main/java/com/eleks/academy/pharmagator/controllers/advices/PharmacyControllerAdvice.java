5a3faf3397494a26f2eb0ad3ef6d0af421abdf17
Volodymyr Holiev <vovagolev23@gmail.com>
2021-10-25 13:57:17 +0300

    Bug fix

--- api/src/main/java/com/eleks/academy/pharmagator/controllers/advices/PharmacyControllerAdvice.java
+++ api/src/main/java/com/eleks/academy/pharmagator/controllers/advices/PharmacyControllerAdvice.java
@@ -28,6 +28,6 @@ public class PharmacyControllerAdvice {
 
         responseBody.put("statusCode",e.getRawStatusCode());
 
-        return ResponseEntity.ok(responseBody);
+        return ResponseEntity.badRequest().body(responseBody);
     }
 }
