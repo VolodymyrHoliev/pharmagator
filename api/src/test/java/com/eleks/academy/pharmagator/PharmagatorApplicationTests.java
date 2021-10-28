16d52abb357403d7b935f2393c035eb117b5d6bf
Volodymyr Holiev <vovagolev23@gmail.com>
2021-10-28 16:16:31 +0300

    PriceController end-to-end tests PriceService integration tests

--- api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
+++ api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
@@ -1,16 +1,35 @@
 package com.eleks.academy.pharmagator;
 
+import com.eleks.academy.pharmagator.entities.Pharmacy;
+import com.eleks.academy.pharmagator.repositories.PharmacyRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class PharmagatorApplicationTests {
 
+    @Autowired
+    private PharmacyRepository pharmacyRepository;
+
     @Test
     void contextLoads() {
+
+
+    }
+
+    @Test
+    public void test(){
+
+        List<Pharmacy> all = pharmacyRepository.findAll();
+
+        assertEquals(1,all.size());
     }
 
 }
