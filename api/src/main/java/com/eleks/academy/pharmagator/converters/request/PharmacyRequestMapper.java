7848a7840ca8a715b6617d0ea2d8930adeee1dd3
Volodymyr Holiev <vovagolev23@gmail.com>
2021-10-29 18:41:23 +0300

    Added validation for PharmacyController

--- api/src/main/java/com/eleks/academy/pharmagator/converters/request/PharmacyRequestMapper.java
+++ api/src/main/java/com/eleks/academy/pharmagator/converters/request/PharmacyRequestMapper.java
@@ -2,14 +2,19 @@ package com.eleks.academy.pharmagator.converters.request;
 
 import com.eleks.academy.pharmagator.controllers.requests.PharmacyRequest;
 import com.eleks.academy.pharmagator.entities.Pharmacy;
+import com.eleks.academy.pharmagator.exceptions.NotNullConstraintViolationException;
 import org.springframework.stereotype.Component;
+
+import javax.validation.Valid;
 
 @Component
 public class PharmacyRequestMapper implements RequestToEntityConverter<PharmacyRequest, Pharmacy> {
 
     @Override
-    public Pharmacy toEntity(PharmacyRequest requestObject) {
-
+    public Pharmacy toEntity(@Valid PharmacyRequest requestObject) {
+        if(requestObject == null){
+            throw new NotNullConstraintViolationException("PharmacyRequest can`t be null");
+        }
         String title = requestObject.getTitle();
 
         String medicineLinkTemplate = requestObject.getMedicineLinkTemplate();
