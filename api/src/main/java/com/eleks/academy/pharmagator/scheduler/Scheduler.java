6d11d47b4a4fd5419f6d02692367d394d44592ad
Volodymyr Holiev <vovagolev23@gmail.com>
2021-11-02 01:57:02 +0200

    Added SchedulerService

--- api/src/main/java/com/eleks/academy/pharmagator/scheduler/Scheduler.java
+++ api/src/main/java/com/eleks/academy/pharmagator/scheduler/Scheduler.java
@@ -2,7 +2,8 @@ package com.eleks.academy.pharmagator.scheduler;
 
 import com.eleks.academy.pharmagator.dataproviders.DataProvider;
 import com.eleks.academy.pharmagator.dataproviders.dto.MedicineDto;
-import com.eleks.academy.pharmagator.repositories.MedicineDataService;
+import com.eleks.academy.pharmagator.entities.Pharmacy;
+import com.eleks.academy.pharmagator.services.SchedulerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,27 +11,23 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class Scheduler {
+
     private final List<DataProvider> dataProviders;
-    private final MedicineDataService medicineDataService;
+
+    private SchedulerService schedulerService;
 
     @Scheduled(fixedDelay = 120, timeUnit = TimeUnit.SECONDS)
     public void schedule() {
-        dataProviders.forEach(dataProvider -> {
-            Stream<MedicineDto> medicineDtoStream = dataProvider.loadData();
-            medicineDtoStream
-                    .forEach(this::storeToDatabase);
-        });
+        //TODO save data in the database
     }
 
-    private void storeToDatabase(MedicineDto dto) {
+    private void storeToDatabase(MedicineDto dto, Pharmacy pharmacy) {
 
-//        medicineDataService.saveMedicine(dto);
-//        medicineDataService.savePrice(dto);
+        schedulerService.storeMedicineDto(dto, pharmacy);
     }
 }
