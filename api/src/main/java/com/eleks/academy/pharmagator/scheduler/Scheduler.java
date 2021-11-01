b20a28f55304633434e90a33fc78477401060b69
Volodymyr Holiev <vovagolev23@gmail.com>
2021-11-02 01:57:02 +0200

    Added SchedulerService

--- api/src/main/java/com/eleks/academy/pharmagator/scheduler/Scheduler.java
+++ api/src/main/java/com/eleks/academy/pharmagator/scheduler/Scheduler.java
@@ -2,6 +2,7 @@ package com.eleks.academy.pharmagator.scheduler;
 
 import com.eleks.academy.pharmagator.converters.medicine_dto.MedicineDtoMapper;
 import com.eleks.academy.pharmagator.dataproviders.PharmacyDataProvider;
+import com.eleks.academy.pharmagator.dataproviders.aptslav.AptslavDataProvider;
 import com.eleks.academy.pharmagator.dataproviders.dto.MedicineDto;
 import com.eleks.academy.pharmagator.entities.Medicine;
 import com.eleks.academy.pharmagator.entities.Pharmacy;
@@ -9,6 +10,7 @@ import com.eleks.academy.pharmagator.entities.Price;
 import com.eleks.academy.pharmagator.repositories.MedicineRepository;
 import com.eleks.academy.pharmagator.repositories.PharmacyRepository;
 import com.eleks.academy.pharmagator.repositories.PriceRepository;
+import com.eleks.academy.pharmagator.services.SchedulerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,16 +18,17 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class Scheduler {
+
     private final List<PharmacyDataProvider> dataProviders;
-    private PharmacyRepository pharmacyRepository;
-    private MedicineRepository medicineRepository;
-    private PriceRepository priceRepository;
-    private MedicineDtoMapper mapper;
+
+    private SchedulerService schedulerService;
 
     @Scheduled(fixedDelay = 120, timeUnit = TimeUnit.SECONDS)
     public void schedule() {
@@ -38,12 +41,7 @@ public class Scheduler {
     }
 
     private void storeToDatabase(MedicineDto dto, Pharmacy pharmacy) {
-        Medicine medicine = mapper.toMedicineEntity(dto);
-        medicineRepository.saveAndFlush(medicine);
-        Price price = mapper.toPriceEntity(dto);
 
-        price.setPharmacyId(pharmacy.getId());
-        price.setMedicineId(medicine.getId());
-        priceRepository.save(price);
+        schedulerService.storeMedicineDto(dto, pharmacy);
     }
 }
