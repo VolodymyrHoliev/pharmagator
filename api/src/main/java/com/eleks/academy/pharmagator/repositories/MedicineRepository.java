8bea86bebb92a5832f1162e0ac58309a3a4eca07
Volodymyr Holiev <vovagolev23@gmail.com>
2021-11-04 17:00:10 +0200

    Picked valid repositories version

--- api/src/main/java/com/eleks/academy/pharmagator/repositories/MedicineRepository.java
+++ api/src/main/java/com/eleks/academy/pharmagator/repositories/MedicineRepository.java
@@ -2,6 +2,7 @@ package com.eleks.academy.pharmagator.repositories;
 
 import com.eleks.academy.pharmagator.entities.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.Optional;
 @Repository
 public interface MedicineRepository extends JpaRepository<Medicine, Long> {
 
+    @Query("SELECT medicine FROM Medicine medicine")
     <T> List<T> findAll(Class<T> returnType);
 
     <T> Optional<T> findById(Long id, Class<T> returnType);
--- api/src/main/java/com/eleks/academy/pharmagator/repositories/PriceRepository.java
+++ api/src/main/java/com/eleks/academy/pharmagator/repositories/PriceRepository.java
@@ -3,6 +3,7 @@ package com.eleks.academy.pharmagator.repositories;
 import com.eleks.academy.pharmagator.entities.Price;
 import com.eleks.academy.pharmagator.entities.PriceId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,15 +12,14 @@ import java.util.Optional;
 @Repository
 public interface PriceRepository extends JpaRepository<Price, PriceId> {
 
-    boolean existsByMedicineIdAndPharmacyId(Long medicineId, Long pharmacyId);
+    void deleteByMedicineIdAndPharmacyId(Long medicineId,
+                                         Long pharmacyId);
 
-    <T> Optional<T> deleteByMedicineIdAndPharmacyId(Long medicineId,
-                                                    Long pharmacyId, Class<T> returnType);
-
+    @Query("SELECT price FROM Price price")
     <T> List<T> findAll(Class<T> returnType);
 
-    <T> Optional<T> findAllByMedicineIdAndPharmacyId(Long medicineId,
-                                                     Long pharmacyId, Class<T> returnType);
+    <T> Optional<T> findByMedicineIdAndPharmacyId(Long medicineId,
+                                                  Long pharmacyId, Class<T> returnType);
 
 
 }
