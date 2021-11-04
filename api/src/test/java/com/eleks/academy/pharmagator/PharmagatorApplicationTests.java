02e65a4c326a06e7a74ea3d2d338380191e14291
Volodymyr Holiev <vovagolev23@gmail.com>
2021-11-04 16:25:58 +0200

    Added @ActivesProfiles import statement in PharmagatorApplicationTests

--- api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
+++ api/src/test/java/com/eleks/academy/pharmagator/PharmagatorApplicationTests.java
@@ -2,6 +2,7 @@ package com.eleks.academy.pharmagator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
