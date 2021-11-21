a24149670087b721d69978e7d7045ea88922bd0a
Volodymyr Holiev <vovagolev23@gmail.com>
2021-11-21 21:11:22 +0200

    Added UnsupportedModelException

---
+++ api/src/main/java/com/eleks/academy/pharmagator/parsers/exceptions/UnsupportedModelException.java
@@ -0,0 +1,10 @@
+package com.eleks.academy.pharmagator.parsers.exceptions;
+
+public class UnsupportedModelException extends RuntimeException {
+
+    @Override
+    public String getMessage() {
+        return "Can't map records from .csv to model with @ParsedCollection annotated fields\n" +
+                "Consider use another parser or remove @ParsedCollection annotation";
+    }
+}
